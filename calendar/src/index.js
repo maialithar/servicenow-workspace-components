4d696585a98f4b6abe921db6f6582b80b1a25f9e
maialithar <cezary.blajszczak@gmail.com>
2020-04-19 17:21:11 +0200

    working calendar widget - implemented tui-calendar

--- calendar/src/index.js
+++ calendar/src/index.js
@@ -1,1 +1,4 @@
 import './x-94086-calendar-component';
+
+const el = document.createElement('x-94086-calendar-component');
+document.body.appendChild(el);
--- calendar/src/x-94086-calendar-component/index.js
+++ calendar/src/x-94086-calendar-component/index.js
@@ -1,15 +1,37 @@
-import {createCustomElement} from '@servicenow/ui-core';
+import {createCustomElement, actionTypes} from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import styles from './styles.scss';
+import view from './view';
+import Calendar from 'tui-calendar';
 
-const view = (state, {updateState}) => {
-	return (
-		<div></div>
-	);
-};
+const {COMPONENT_RENDERED, COMPONENT_ERROR_THROWN} = actionTypes;
 
 createCustomElement('x-94086-calendar-component', {
 	renderer: {type: snabbdom},
 	view,
-	styles
+	styles,
+	actionHandlers: {
+		[COMPONENT_RENDERED]: {
+			effect({state, action: {payload: {host}}}) {
+				var calendar = new Calendar(host.shadowRoot.childNodes[0].childNodes[1], {
+					defaultView: 'month',
+					taskView: true,
+					template: {
+						monthDayname: function(dayname) {
+							return '<span class="calendar-week-dayname-name">' + dayname.label + '</span>';
+						}
+					},
+					isReadOnly: true,
+					MonthOptions: {
+						startDayOfWeek: 1,
+						narrowWeekend: true,
+						visibleScheduleCount: 3
+					}
+				});
+			}
+		},
+		[COMPONENT_ERROR_THROWN]: ({action: {payload}}) => {
+			console.log(payload)
+		}
+	}
 });
